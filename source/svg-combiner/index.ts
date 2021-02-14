8199dcb6ac5e29a23cd9280db4cc84e8cdc7b20e
Vladimirs Nordholm <v@vladde.net>
2021-02-14 04:26:15 +0100

    project cleanup

--- source/svg-combiner/index.ts
+++ source/svg-combiner/index.ts
@@ -1,38 +1,70 @@
 /* eslint-disable no-restricted-syntax */
-import * as fs from 'fs';
+import * as fs from 'fs'
 
 export type Part = {
-  file: string
-  x: number
-  y: number
-  scale?: number
+  file: string,
+  x: number,
+  y: number,
+  scale?: number,
 }
 
 export type DocumentConfiguration = {
-  width: number
-  height: number
-  parts: Part[]
+  width: number,
+  height: number,
+  parts: Part[],
 }
 
 /// generate svg data
 export function generate(config: DocumentConfiguration): string {
-  let data: string = '';
+  let data = ''
 
   // fixme use some sort of dom generator
   // this is beyond horrible
   // ...
   // but it works!
 
-  data += `<svg fill="none" width="${config.width}" height="${config.height}" xmlns="http://www.w3.org/2000/svg">`;
+  data += `<svg fill="none" width="${config.width}" height="${config.height}" xmlns="http://www.w3.org/2000/svg">`
 
   for (const part of config.parts) {
     //
-    data += `<g transform="translate(${part.x},${part.y})${part.scale ? ` scale(${part.scale})` : ''}">`;
-    data += fs.readFileSync(part.file);
-    data += '</g>';
+    data += `<g transform="translate(${part.x},${part.y})${part.scale ? ` scale(${part.scale})` : ''}">`
+    data += fs.readFileSync(part.file)
+    data += '</g>'
   }
 
-  data += '</svg>';
+  data += '</svg>'
 
-  return data;
+  return data
 }
+
+(() => {
+  const config: DocumentConfiguration = {
+    width: 800,
+    height: 230,
+    parts: [
+      {
+        file: './body.svg',
+        x: 10,
+        y: 5,
+      },
+      {
+        file: './tuke.svg',
+        x: 618,
+        y: 3,
+      },
+      {
+        file: './north cap.svg',
+        x: 592,
+        y: 3,
+      },
+      {
+        file: './left hand.svg',
+        x: 644,
+        y: 47,
+      },
+    ],
+  }
+
+  const data = generate(config)
+  fs.writeFileSync('output.svg', data)
+})()
