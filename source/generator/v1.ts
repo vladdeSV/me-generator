def1a0da35a094fbd35b4c2bfd63d82c99ab19df
Vladimirs Nordholm <v@vladde.net>
2021-03-11 17:59:56 +0100

    load part map from json file

--- source/generator/v1.ts
+++ source/generator/v1.ts
@@ -1,5 +1,6 @@
 import { prng } from './index'
-import { Part } from '../svg-combiner'
+import { Part, readJsonFileAs } from '../svg-combiner'
+import * as path from 'path'
 
 export function generateV1(rng: prng): Part[] {
   const ids = [
@@ -21,7 +22,11 @@ export function generateV1(rng: prng): Part[] {
     'right hand',
     'left hand',
   ]
-  return convertPartIdsToParts(ids, parts)
+
+  const data = readJsonFileAs<{ [name: string]: [number, number] }>('./me-generator-images/source/position.json')
+  const partMap = generatePartMapFromJson('./me-generator-images/', data)
+
+  return convertPartIdsToParts(ids, partMap)
 }
 
 function generateFacialHairIds(rng: prng): (string | undefined)[] {
@@ -89,102 +94,22 @@ function randomWeightPart(rng: prng, parts: (WeightedPart | PartId | undefined)[
   return selectRandomElementByWeight(rng, partIds, partWeights)
 }
 
-const parts: PartMap = {
-  'torso': {
-    file: './me-generator-images/torso.svg',
-    x: 399.4,
-    y: 94.8,
-  },
-  'right leg': {
-    file: './me-generator-images/right leg.svg',
-    x: 336.1,
-    y: 26.5,
-  },
-  'left leg': {
-    file: './me-generator-images/left leg.svg',
-    x: 47.7,
-    y: 175.4,
-  },
-  'left arm': {
-    file: './me-generator-images/left arm.svg',
-    x: 683.9,
-    y: 137.3,
-  },
-  'right arm': {
-    file: './me-generator-images/right arm.svg',
-    x: 490.9,
-    y: 80.9,
-  },
-  'head': {
-    file: './me-generator-images/head.svg',
-    x: 654.3,
-    y: 51.7,
-  },
-  'underwear 1': {
-    file: './me-generator-images/underwear 1.svg',
-    x: 403.9,
-    y: 124.9,
-  },
-  'duality socks': {
-    file: './me-generator-images/duality socks.svg',
-    x: 46.7,
-    y: 146.3,
-  },
-  'beige cargo pants': {
-    file: './me-generator-images/beige cargo pants.svg',
-    x: 147.5,
-    y: 21.8,
-  },
-  'necklace': {
-    file: './me-generator-images/necklace.svg',
-    x: 641.4,
-    y: 113.6,
-  },
-  'ltt crewneck': {
-    file: './me-generator-images/ltt crewneck.svg',
-    x: 471.8,
-    y: 79.1,
-  },
-  'beard bigger': {
-    file: './me-generator-images/beard bigger.svg',
-    x: 649.5,
-    y: 86.3,
-  },
-  'beard 3mm': {
-    file: './me-generator-images/beard 3mm.svg',
-    x: 649.5,
-    y: 86.3,
-  },
-  'hair roff': {
-    file: './me-generator-images/hair roff.svg',
-    x: 671.5,
-    y: 36.3,
-  },
-  'hair 1cm': {
-    file: './me-generator-images/hair 1cm.svg',
-    x: 675.3,
-    y: 51.7,
-  },
-  'ltt touke': {
-    file: './me-generator-images/ltt touke.svg',
-    x: 664.1,
-    y: 29.7,
-  },
-  'minibrills': {
-    file: './me-generator-images/minibrills.svg',
-    x: 671.1,
-    y: 77,
-  },
-  'left hand': {
-    file: './me-generator-images/left hand.svg',
-    x: 695.2,
-    y: 84,
-  },
-  'right hand': {
-    file: './me-generator-images/right hand.svg',
-    x: 539.4,
-    y: 205.8,
-  },
+function generatePartMapFromJson(basePath: string, data: { [name: string]: [number, number] }): PartMap {
+  const partMap: PartMap = {}
+
+  for (const partId in data) {
+    if (Object.prototype.hasOwnProperty.call(data, partId)) {
+      const position = data[partId]
+
+      partMap[partId] = {
+        file: path.join(basePath, `${partId}.svg`),
+        x: position[0],
+        y: position[1],
+      }
+    }
+  }
+
+  return partMap
 }
 
 function convertPartIdsToParts(ids: (string | undefined)[], parts: PartMap): Part[] {
