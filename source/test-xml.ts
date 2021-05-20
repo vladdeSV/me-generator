c52b8a685e1b49e2d5c109b6b74d0041ca5dddf1
Vladimirs Nordholm <v@vladde.net>
2021-05-20 21:47:32 +0200

    working svg -> js -> svg

--- source/test-xml.ts
+++ source/test-xml.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-floating-promises */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 /* eslint-disable @typescript-eslint/restrict-template-expressions */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
@@ -5,60 +6,62 @@
 /* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import * as xml2js from 'xml2js'
 import * as fs from 'fs'
+import { inspect } from 'util'
 
-const a = ((async (): Promise<void> => {
+(async () => {
   
   const pseudoUniqueSvgId = Math.random().toString(36).substr(2, 5)
-  const svgData = fs.readFileSync('../me-generator-images/exported/ltt crewneck.svg')
+  const svgData = fs.readFileSync('../me-generator-images/exported/beard 3mm.svg')
     .toString('utf8')
     .replace(/<\?xml.*?\?>/, '')
     .replace(/<!DOCTYPE.*?>/, '')
     .replace(/width="100%" height="100%" viewBox="0 0 (\d+) (\d+)"/, 'width="$1" height="$2"')
     .replace(/_clip(\d+)/g, `_clip$1_${pseudoUniqueSvgId}`)
 
-  console.log(svgData)
-
   const options: xml2js.ParserOptions = {
     strict: true,
     explicitArray: false,
+    explicitChildren: true,
     preserveChildrenOrder: true,
   }
 
-  const xml = await xml2js.parseStringPromise(svgData, options)
+  const data = (await xml2js.parseStringPromise(svgData, options)).svg
+  console.log(JSON.stringify(data))
 
-  fs.writeFileSync('./test.svg', buildXmlFromJs(xml.svg, 'svg'))
+  const newXml = buildXmlFromJsOrderedChildren(data)
+  console.log(newXml)
 
-})())
+  fs.writeFileSync('./test.svg', `<?xml version="1.0" encoding="UTF-8" standalone="no"?>
+  <!DOCTYPE svg PUBLIC "-//W3C//DTD SVG 1.1//EN" "http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd">
+  ` + newXml)
 
-function buildXmlFromJs(xml: any, tagName: string): string {
+})()
 
-  console.log(xml)
-
-  if(Array.isArray(xml)) {
-    return xml.map(x => buildXmlFromJs(x, tagName)).join('')
-  }
-
-  if(typeof xml !== 'object') {
-    return xml
-  }
-
-  const attributes: undefined | Record<string, any> = xml.$
-  let attributesString: undefined | string
-
-  if(attributes) {
-    attributesString = Object.keys(attributes).map(key => `${key}="${attributes[key]}"`).join(' ')
-    delete xml.$
-  }
-
-  const xmlTags = Object.keys(xml)
-
-  if(xmlTags.length) {
-    return `<${tagName}${attributesString ? ` ${attributesString}` : ''}>
-  ${ xmlTags.map(tagName => buildXmlFromJs(xml[tagName], tagName)).join('\n') }
-</${tagName}>`
-  } else {
-    return `<${tagName}${attributesString ? ` ${attributesString}` : ''}/>`
-  }
+type XmlTag = {
+  $?: {[name: string]: XmlTag}
+  $$?: XmlTag[]
+  '#name': string
 }
 
-console.log(a)
+/*
+  Assuming XML parser options are:
+    - strict: true,
+    - explicitArray: false,
+    - explicitChildren: true,
+    - preserveChildrenOrder: true,
+*/
+function buildXmlFromJsOrderedChildren(xml: XmlTag): string {
+  
+  const tagName = xml['#name']
+  const attributes = xml.$ ?? {}
+  const children = xml.$$
+
+  const attributesString = Object.keys(attributes).map(key => `${key}="${attributes[key]}"`).join(' ')
+
+  if(children === undefined) {
+    return `<${tagName} ${attributesString}/>`
+  }
+
+  return `<${tagName} ${attributesString}>${children.map(child => buildXmlFromJsOrderedChildren(child)).join('')}</${tagName}>`
+}
+
